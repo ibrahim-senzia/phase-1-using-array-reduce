0d98abdedd9d52171dc5e8052b5fb668f62a1362
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-30 13:24:50 +0300

    array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const BatteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+//reducer() batteries should have 'totalBatteries'
+const batteryBatches = [
+    {batch:1, amount:100},
+    {batch:2, amount:200},
+    {batch:3, amount:300}
+];
+//reducer should have a number as a result
+const totalBatteries = batteryBatches.reduce((total, currentBatch) =>{
+    return total + currentBatch.amount;
+}, 0);
+// reducer should have made sum of all assembled batteries
+console.log(totalBatteries);
+
+
